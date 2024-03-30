d8773201013e2858fdad306afa470a00fb4c5ce3
Allejandro <allejandro.empresarial@gmail.com>
2024-03-30 10:53:41 -0300

    Arquivo de teste do TAD

---
+++ 14. Listas encadeadas/Circulares S_E/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "circular_s_e.h"
+
+int main() {
+    No* no = cria_no(8);
+    Listac* l = cria_listac();
+    insere_listac(l, 1);
+    insere_listac(l, 2);
+    insere_listac(l, 3);
+    imprime_listac(l);
+    printf("\n");
+    remove_listac(l, 3);
+    modifica_listac(l, 2, 6);
+    imprime_listac(l);
+    printf("\n");
+    insere_listac(l, 0);
+    insere_listac(l, 8);
+    imprime_listac(l);
+    libera_listac(&l);
+    imprime_listac(l);
+}
